d357738b69333cee3071b3ccec6cce90c9952ef5
tranvlnh <92441399+tranvlnh@users.noreply.github.com>
2026-07-29 12:17:17 +0700

    style(movingplatform): reformat moving platform script
    - remove unnecessary braces and spaces
    - remove redundant access modifiers
    - add null checks for player transform

--- Assets/_Project/Scripts/Platforms/MovingPlatform.cs
+++ Assets/_Project/Scripts/Platforms/MovingPlatform.cs
@@ -2,59 +2,61 @@ using UnityEngine;
 
 // ReSharper disable CheckNamespace
 
-public class MovingPlatform : MonoBehaviour
-{
-    [Header("Movement Settings")] [SerializeField]
-    private Transform[] waypoints;
+public class MovingPlatform : MonoBehaviour {
 
-    [SerializeField] private float speed = 3f;
-    [SerializeField] private float waitTime = 1f;
-    private int _index;
-    private bool _isWaiting;
-    private float _waitTimer;
+	[Header("Movement Settings")]
+	[SerializeField]
+	Transform[] waypoints;
 
-    private void FixedUpdate()
-    {
-        if (_isWaiting)
-        {
-            _waitTimer -= Time.fixedDeltaTime;
-            if (_waitTimer <= 0)
-                _isWaiting = false;
-            return;
-        }
+	[SerializeField] float speed = 3f;
+	[SerializeField] float waitTime = 1f;
 
-        var target = waypoints[_index].position;
-        if (Vector3.Distance(transform.position, target) > 0.01f)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
-        }
-        else
-        {
-            _isWaiting = true;
-            _waitTimer = waitTime;
-            _index = (_index + 1) % waypoints.Length;
-        }
-    }
+	int _index;
+	bool _isWaiting;
+	float _waitTimer;
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player")) other.transform.SetParent(transform);
-    }
+	void FixedUpdate()
+	{
+		if (_isWaiting)
+		{
+			_waitTimer -= Time.fixedDeltaTime;
+			if (_waitTimer <= 0)
+				_isWaiting = false;
+			return;
+		}
 
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player")) other.transform.SetParent(null);
-    }
+		var target = waypoints[_index].position;
+		if (Vector3.Distance(transform.position, target) > 0.01f)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, target, speed*Time.fixedDeltaTime);
+		}
+		else
+		{
+			_isWaiting = true;
+			_waitTimer = waitTime;
+			_index = (_index + 1)%waypoints.Length;
+		}
+	}
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        for (var i = 0; i < waypoints.Length; i++)
-        {
-            if (waypoints[i] == null) continue;
-            Gizmos.DrawWireSphere(waypoints[i].position, 0.1f);
-            var nextWaypoint = waypoints[(i + 1) % waypoints.Length];
-            if (nextWaypoint != null) Gizmos.DrawLine(waypoints[i].position, nextWaypoint.position);
-        }
-    }
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player")) other.transform.SetParent(transform);
+	}
+
+	void OnCollisionExit2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player")) other?.transform?.SetParent(null);
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		for (var i = 0; i < waypoints.Length; i++)
+		{
+			if (waypoints[i] == null) continue;
+			Gizmos.DrawWireSphere(waypoints[i].position, 0.1f);
+			var nextWaypoint = waypoints[(i + 1)%waypoints.Length];
+			if (nextWaypoint != null) Gizmos.DrawLine(waypoints[i].position, nextWaypoint.position);
+		}
+	}
 }
